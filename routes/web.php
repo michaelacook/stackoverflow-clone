864f4fa369b560862ba6f89736b7a72884075f63
Michael A. Cook <mcook0775@gmail.com>
2021-09-30 19:46:46 -0400

    fix deleted account route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AccountController;
-use App\Http\Controllers\AnswerController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
 use Illuminate\Foundation\Application;
@@ -29,6 +28,7 @@ Route::get('/', function () {
 });
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/account', [AccountController::class, 'index']);
 
 Route::post('/answers/upvote', [AnswerController::class, 'upVote']);
 Route::post('/answers/downvote', [AnswerController::class, 'downVote']);
