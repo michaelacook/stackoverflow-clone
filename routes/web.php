5d8bdc0b4298c928c3dd554a2d5d19abe11c0c72
Michael A. Cook <mcook0775@gmail.com>
2021-11-20 18:39:13 -0500

    remove default welcome view

--- routes/web.php
+++ routes/web.php
@@ -22,15 +22,6 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
 Route::get('/', [QuestionController::class, 'getQuestionsByWatched']);
 Route::get('/account', [AccountController::class, 'index']);
 
