08b62207b16e65d653f03578ff892817afc0aa9f
Michael A. Cook <mcook0775@gmail.com>
2021-11-20 20:10:18 -0500

    create new notification

---
+++ app/Notifications/NewQuestionCommentNotification.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+use App\Models\Question;
+use App\Models\Comment;
+
+class NewQuestionCommentNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Question $question, Comment $comment)
+    {
+        $this->question = $question;
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'type' => 'comment',
+            'title' => $this->question->title,
+            'preview' => substr($this->answer->body, 0, 50),
+            'url' => '/questions' . '/' . $this->question->slug . '#' . $this->comment->id
+        ];
+    }
+}
