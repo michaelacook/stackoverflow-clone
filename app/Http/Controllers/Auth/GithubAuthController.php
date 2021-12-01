70d07248949048d24a0923fd475b4d6d2253f663
Michael A. Cook <mcook0775@gmail.com>
2021-11-30 21:54:49 -0500

    move github auth controller to auth folder

--- app/Http/Controllers/Auth/GithubAuthController.php
+++ app/Http/Controllers/Auth/GithubAuthController.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Str;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 use Laravel\Socialite\Facades\Socialite;
 
 class GithubAuthController extends Controller
--- routes/auth.php
+++ routes/auth.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
-use App\Http\Controllers\GithubAuthController;
+use App\Http\Controllers\Auth\GithubAuthController;
 use App\Http\Controllers\Auth\FacebookAuthController;
 use Illuminate\Support\Facades\Route;
 
