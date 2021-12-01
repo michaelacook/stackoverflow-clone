2aa5fa46a37b777671e71e7fc5feb2fac55107b8
Michael A. Cook <mcook0775@gmail.com>
2021-11-30 19:44:40 -0500

    minor fixes, add comments

--- app/Http/Controllers/GithubAuthController.php
+++ app/Http/Controllers/GithubAuthController.php
@@ -3,20 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
 
 class GithubAuthController extends Controller
 {
+    /**
+     * Redirect user to Github to confirm identity
+     */
     public function redirect()
     {
         return Socialite::driver('github')->redirect();
     }
 
+    /**
+     * Authenticate a user using their Github credentials 
+     * 
+     * If the user does not already exist, create a new user
+     */
     public function authenticate()
     {
         $user = Socialite::driver('github')->user();
