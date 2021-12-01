36c5b28a0409b6bcb53a01370d41043b94374897
Michael A. Cook <mcook0775@gmail.com>
2021-11-30 19:36:42 -0500

    undo last changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,10 +21,7 @@ class CreateUsersTable extends Migration
             $table->boolean('isAdmin')->default(0);
             $table->boolean('isModerator')->default(0);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->string('githubToken')->nullable();
-            $table->string('facebookToken')->nullable();
-            $table->string('googleToken')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
